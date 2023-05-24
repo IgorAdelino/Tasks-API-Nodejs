b760b42d8025f794a913d4ff58cb18533c187a92
Igor Adelino <igoradelino@gmail.com>
2023-05-23 23:37:57 -0300

    first commit

---
+++ src/middlewares/json.js
@@ -0,0 +1,13 @@
+export async function json(req, res){
+  const Buffers = []
+
+  for await (const chunk of req){
+    Buffers.push(chunk)
+  }
+  try {
+    req.body = JSON.parse(Buffer.concat(Buffers).toString())
+  } catch (error) {
+    req.body - null
+  }
+  res.setHeader('Content-type', 'application/json')
+}
